4d95d71d5ec00b0635b0cb45d580c2d7daa7a61c
Mirko Kiefer <mail@mirkokiefer.com>
2011-10-15 19:44:51 +0200

    refactor scanner to manage states centrally

--- modeling.erl
+++ modeling.erl
@@ -9,35 +9,42 @@ testCode() ->
   SourceString = binary:bin_to_list(Data).
 
 test() ->
-  scan(testCode()).
+  lists:reverse(scan(start, false, testCode(), [])).
 
-scan(String) ->
-  lists:reverse(scan_entity_name(String, "", [])).
+scan(LastState, MatchSucceeded, RestString, Tokens) ->
+  case {LastState, MatchSucceeded} of
+    {start, _} -> scan_entity_name(RestString, "", Tokens);
+    {entity_name, true} -> scan_entity_type(RestString, "", Tokens);
+    {entity_type, true} -> scan_attribute_name(RestString, "", Tokens);
+    {attribute_name, true} -> scan_attribute_value(RestString, "", Tokens);
+    {attribute_name, false} -> Tokens;
+    {attribute_value, true} -> scan_attribute_name(RestString, "", Tokens)
+  end.
 
 scan_entity_name([First|Rest], TryMatch, Tokens) ->
   case [First] of
-    "(" -> scan_entity_type(Rest, "", [entity_name(lists:reverse(TryMatch)) | Tokens]);
+    "(" -> scan(entity_name, true, Rest, [entity_name(lists:reverse(TryMatch)) | Tokens]);
     Any -> scan_entity_name(Rest, [First|TryMatch], Tokens)
   end.
 
 scan_entity_type([First|Rest], TryMatch, Tokens) ->
   case [First] of
-    ")" -> scan_entity_attribute_name(Rest, "", [entity_type(lists:reverse(TryMatch)) | Tokens]);
+    ")" -> scan(entity_type, true, Rest, [entity_type(lists:reverse(TryMatch)) | Tokens]);
     Any -> scan_entity_type(Rest, [First|TryMatch], Tokens)
   end.
 
-scan_entity_attribute_name([First|Rest], TryMatch, Tokens) ->
+scan_attribute_name([First|Rest], TryMatch, Tokens) ->
   case [First] of
-    ":" -> scan_entity_attribute_value(Rest, "", [entity_attribute_name(lists:reverse(TryMatch)) | Tokens]);
-    "." -> Tokens;
-    Any -> scan_entity_attribute_name(Rest, [First|TryMatch], Tokens)
+    ":" -> scan(attribute_name, true, Rest, [attribute_name(lists:reverse(TryMatch)) | Tokens]);
+    "." -> scan(attribute_name, false, Rest, Tokens);
+    Any -> scan_attribute_name(Rest, [First|TryMatch], Tokens)
   end.
 
-scan_entity_attribute_value([First|Rest], TryMatch, Tokens) ->
+scan_attribute_value([First|Rest], TryMatch, Tokens) ->
   case [First] of
-    "," -> scan_entity_attribute_name(Rest, "", [entity_attribute_value(lists:reverse(TryMatch)) | Tokens]);
-    "." -> Tokens;
-    Any -> scan_entity_attribute_value(Rest, [First|TryMatch], Tokens)
+    "," -> scan(attribute_value, true, Rest, [attribute_value(lists:reverse(TryMatch)) | Tokens]);
+    "." -> scan(attribute_value, true, [First|Rest], [attribute_value(lists:reverse(TryMatch)) | Tokens]);
+    Any -> scan_attribute_value(Rest, [First|TryMatch], Tokens)
   end.
 
 entity_name(String) ->
@@ -46,13 +53,10 @@ entity_name(String) ->
 entity_type(String) ->
   #token{string=String, type="EntityType"}.
 
-entity_attribute_name(String) ->
+attribute_name(String) ->
   #token{string=String, type="AttributeName"}.
 
-entity_attribute_value(String) ->
+attribute_value(String) ->
   #token{string=String, type="AttributeValue"}.
-
-remove_ignorable_chars(String) ->
-  .
 
 ignorableChars() -> ["\n", " "].
