e7566383d83b2d04170c88bad64cefbf7cb730c2
Mirko Kiefer <mail@mirkokiefer.com>
2011-10-16 03:06:51 +0200

    further optimized

--- modeling.erl
+++ modeling.erl
@@ -1,43 +1,92 @@
 -module(modeling).
 
--export([test/0]).
+-export([test/0, test/1, test_code/1, print/0]).
 
 -record(token, {string, type}).
+-record(state, {optional=false, states, string, tokens}).
 
-testCode() ->
-  {ok, Data} = file:read_file("./test.ml"),
+test_code(File) ->
+  {ok, Data} = file:read_file("./" ++ File ++ ".ml"),
   binary:bin_to_list(Data).
 
-test() ->
-  lists:reverse(scan([start], false, testCode(), [])).
+print() ->
+  lists:foreach(fun(Each) -> io:format("~p~n", [Each]) end, test_code("test1")).
 
-scan(States, MatchSucceeded, RestString, Tokens) ->
-  io:format("~p~n~p~n~p~n~p~n~n", [States, MatchSucceeded, RestString, Tokens]),
-  NewState = case {States, MatchSucceeded} of
-    {[start|_], _} -> entity_name;
-    {[entity_name|_], true} -> left_bracket;
-    {[left_bracket, entity_name|_], true} -> entity_type;
-    {[entity_type|_], true} -> right_bracket;
-    {[right_bracket,entity_type|_], true} -> newline;
-    {[newline, right_bracket, entity_type|_], _} -> attribute_name;
-    {[attribute_name|_], true} -> colon;
-    {[attribute_name|_], false} -> dot;
-    {[colon, attribute_name|_], true} -> attribute_value;
-    {[attribute_value|_], true} -> comma;
-    {[comma, attribute_value|_], true} -> attribute_name;
-    {[comma, attribute_value|_], false} -> dot;
-    {[dot|_], _} -> success
-  end,
-  case NewState of
-    success -> Tokens;
-    _ -> eval(NewState, RestString, States, Tokens)
+test() -> test("test").
+
+test(File) ->
+  {success, Tokens, _RestString} = scan([start], test_code(File), []),
+  FilteredTokens = filter_tokens(Tokens, [spaces, newline, comma, colon, left_bracket, right_bracket, eof]),
+  io:format("tokens: ~p~n", [lists:reverse(FilteredTokens)]).
+
+filter_tokens(Tokens, FilterTypes) ->
+  [Token || Token=#token{type=Type} <- Tokens, lists:member(Type, FilterTypes) == false].
+
+grammar(State) ->
+  case State of
+    start -> [entities];
+    entities ->[entity, {any, [next_entity, stop]}];
+    next_entity -> [newline, entities];
+    entity -> [entity_name, left_bracket, entity_type, right_bracket, {any, [dot, attributes]}];
+    attributes -> [newline, attribute];
+    attribute -> [{optional, spaces}, attribute_name, colon,
+      {optional, spaces}, attribute_value, {any, [next_attribute, dot]}];
+    next_attribute -> [comma, newline, attribute];
+    stop -> [{optional, eof}];
+    _ -> undefined
   end.
 
-eval(State, String, States, Tokens) ->
-  {Success, Token, RestString} = scan_type(String, State),
-  case Success of
-    true -> scan([State|States], true, RestString, [Token|Tokens]);
-    false -> scan([State|States], false, String, Tokens)
+scan([{optional, OptionalState}|Rest], String, Tokens) ->
+  io:format("scan optional: ~p~n~p~n~p~n~n", [OptionalState, String, Tokens]),
+  case grammar(OptionalState) of
+    undefined ->
+      case scan_type(String, OptionalState) of
+        {true, Token, RestString} -> scan(Rest, RestString, [Token|Tokens]);
+        {false, _, _} -> scan(Rest, String, Tokens)
+      end;
+    States ->
+      case scan(States, String, Tokens) of
+        {error, _} -> scan(Rest, String, Tokens);
+        {success, NewTokens, RestString} -> scan(Rest, RestString, NewTokens)
+      end
+  end;
+  
+
+scan([{any, []}|_Rest], _String, _Tokens) ->
+  {error, "no match in any state"};
+
+scan([{any, [FirstState|RestAny]}|Rest], String, Tokens) ->
+  io:format("scan any: ~p~n~p~n~p~n~n", [FirstState, String, Tokens]),
+  case grammar(FirstState) of
+    undefined ->
+      case scan_type(String, FirstState) of
+        {true, Token, RestString} -> scan(Rest, RestString, [Token|Tokens]);
+        {false, _, _} -> scan([{any, RestAny}|Rest], String, Tokens)
+      end;
+    States ->
+      case scan(States, String, Tokens) of
+        {error, _} -> scan([{any, RestAny}|Rest], String, Tokens);
+        {success, NewTokens, RestString} -> scan(Rest, RestString, NewTokens)
+      end
+  end;
+
+scan([], String, Tokens) ->
+  {success, Tokens, String};
+
+scan([FirstState|Rest], String, Tokens) ->
+  io:format("scan: ~p~n~p~n~p~n~n", [FirstState, String, Tokens]),
+  case grammar(FirstState) of
+    undefined ->
+      case scan_type(String, FirstState) of
+        {true, Token, RestString} -> scan(Rest, RestString, [Token|Tokens]);
+        {false, _, _} -> {error, "required state not matched"}
+      end;
+    States ->
+      case scan(States, String, Tokens) of
+        {error, Message} -> {error, Message};
+        {no_match, String, Tokens} -> scan(Rest, String, Tokens);
+        {success, NewTokens, RestString} -> scan(Rest, RestString, NewTokens)
+      end
   end.
 
 scan_type(String, Type) -> scan_type(String, "", Type).
@@ -57,18 +106,20 @@ type_matched(String, Match, Type) ->
 token(String, Type) ->
   #token{string=String, type=Type}.
 
-member(Char, Type) -> lists:member(Char, chars_for_type(Type)).
+member(Char, Type) -> lists:member(Char, primitive_chars(Type)).
 
 name() -> utils:letters() ++ utils:digits() ++ utils:space().
 
-chars_for_type(Type) ->
+primitive_chars(Type) ->
   case Type of  
     entity_name -> name();
     entity_type -> name();
     attribute_name -> name();
     attribute_value -> name();
-    colon -> ": ";
-    comma -> ",\n ";
+    spaces -> " ";
+    eof -> "\n ";
+    colon -> ":";
+    comma -> ",";
     dot -> utils:dot();
     left_bracket -> "(";
     right_bracket -> ")";
--- test.ml
+++ test.ml
@@ -2,3 +2,8 @@ Libya (Country)
   part of: Africa,
   population: 6 Mio,
   headed by: Gaddhafi.
+
+Germany (Country)
+  part of: Europe,
+  population: 86 Mio.
+
